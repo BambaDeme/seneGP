31d0b5ce100e6bea41294030bfc246322f3f5a91
bambademe <cheikhbambademe@gmail.com>
2021-09-03 14:07:06 -0700

    mise en place de la navigation et des components

--- App.js
+++ App.js
@@ -1,4 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import HomeScreen from './components/HomeScreen';
+import SettingsScreen from './components/SettingsScreen';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+
+export default function App() {
+
+  const [signedIn,setSignedIn] = React.useState(false)
+  return !signedIn ? (
+    <>
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} initialParams={{ signedIn:setSignedIn }}/>
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  ) : (
+    <>
+      <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Search" component={HomeScreen} />
+        <Tab.Screen name="Historique" component={SettingsScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+      </NavigationContainer>
+    </>
+  )
+}
+
+
+
+/*import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -18,4 +62,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});*/
